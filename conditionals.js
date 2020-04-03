3a0c4d63d21c30625a950021815fa5a1f1d6d422
grabania <grabania.work@gmail.com>
2020-04-03 23:18:51 +0100

    Add musical groups

--- conditionals.js
+++ conditionals.js
@@ -81,3 +81,30 @@ if (number % 2 === 1) {
 } else {
 	console.log('even');
 }
+
+
+/*
+ * Programming Quiz: Musical Groups
+ */
+
+
+var musicians = 1;
+
+
+if (musicians < 1) {
+	console.log("not a group");
+}
+else if (musicians === 1) {
+	console.log("solo");
+}
+else if (musicians === 2) {
+	console.log("duet");
+}
+else if (musicians === 3) {
+	console.log("trio");
+}
+else if (musicians === 4) {
+	console.log("quartet");
+}
+else {
+	console.log("this is a large group");
