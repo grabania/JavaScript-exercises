23e6e45b27de8e6011314afd8c44acf91aedacc0
grabania <grabania.work@gmail.com>
2020-04-04 21:15:17 +0100

    Create Murder Mystery Quiz

--- conditionals.js
+++ conditionals.js
@@ -108,3 +108,31 @@ else if (musicians === 4) {
 }
 else {
 	console.log("this is a large group");
+
+
+/*
+	* Programming Quiz: Murder Mystery (3-4)
+	*/
+
+var room = "dining room";
+var suspect = "Mr. Parkes";
+
+var weapon = "";
+var solved = false;
+
+if (room === "ballroom" && suspect === "Mr. Kalehoff") {
+	weapon = "poison";
+	solved = true;
+} else if (room === "gallery" && suspect === "Ms. Van Cleve") {
+	weapon = "trophy";
+	solved = true;
+} else if (room === "billiards room" && suspect === "Mrs. Sparr") {
+	weapon = "pool stick";
+	solved = true;
+} else if (room === "dining room" && suspect === "Mr. Parkes") {
+	weapon = "knife";
+	solved = true;
+}
+
+if (solved) {
+	console.log(suspect + " did it in the " + room + " with the " + weapon + "!");
