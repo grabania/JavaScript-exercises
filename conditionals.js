307c359fd4965206d8b4b52f8eb7e2438557b1b7
grabania <grabania.work@gmail.com>
2020-04-04 22:46:51 +0100

    Create logical operation example

--- conditionals.js
+++ conditionals.js
@@ -82,57 +82,56 @@ if (number % 2 === 1) {
 	console.log('even');
 }
 
-
 /*
  * Programming Quiz: Musical Groups
  */
 
-
 var musicians = 1;
 
-
 if (musicians < 1) {
-	console.log("not a group");
+	console.log('not a group');
+} else if (musicians === 1) {
+	console.log('solo');
+} else if (musicians === 2) {
+	console.log('duet');
+} else if (musicians === 3) {
+	console.log('trio');
+} else if (musicians === 4) {
+	console.log('quartet');
+} else {
+	console.log('this is a large group');
 }
-else if (musicians === 1) {
-	console.log("solo");
-}
-else if (musicians === 2) {
-	console.log("duet");
-}
-else if (musicians === 3) {
-	console.log("trio");
-}
-else if (musicians === 4) {
-	console.log("quartet");
-}
-else {
-	console.log("this is a large group");
-
-
 /*
 	* Programming Quiz: Murder Mystery (3-4)
 	*/
 
-var room = "dining room";
-var suspect = "Mr. Parkes";
+var room = 'dining room';
+var suspect = 'Mr. Parkes';
 
-var weapon = "";
+var weapon = '';
 var solved = false;
 
-if (room === "ballroom" && suspect === "Mr. Kalehoff") {
-	weapon = "poison";
+if (room === 'ballroom' && suspect === 'Mr. Kalehoff') {
+	weapon = 'poison';
 	solved = true;
-} else if (room === "gallery" && suspect === "Ms. Van Cleve") {
-	weapon = "trophy";
+} else if (room === 'gallery' && suspect === 'Ms. Van Cleve') {
+	weapon = 'trophy';
 	solved = true;
-} else if (room === "billiards room" && suspect === "Mrs. Sparr") {
-	weapon = "pool stick";
+} else if (room === 'billiards room' && suspect === 'Mrs. Sparr') {
+	weapon = 'pool stick';
 	solved = true;
-} else if (room === "dining room" && suspect === "Mr. Parkes") {
-	weapon = "knife";
+} else if (room === 'dining room' && suspect === 'Mr. Parkes') {
+	weapon = 'knife';
 	solved = true;
 }
 
 if (solved) {
-	console.log(suspect + " did it in the " + room + " with the " + weapon + "!");
+	console.log(suspect + ' did it in the ' + room + ' with the ' + weapon + '!');
+}
+
+var colt = 'not busy';
+var weather = 'nice';
+
+if (colt === 'not busy' && weather === 'nice') {
+	console.log('go to the park');
+}
