11886c902661328527a85c3937e8a3807716ef10
grabania <grabania.work@gmail.com>
2020-04-07 23:31:44 +0100

    Create Winner Game

--- conditionals.js
+++ conditionals.js
@@ -241,3 +241,19 @@ switch (tier) {
 }
 
 console.log(output);
+
+var prize = '';
+
+switch (winner) {
+	case 1:
+		prize += 'a trip for two to the Bahamas and ';
+	case 2:
+		prize += 'a four piece furniture set.';
+		break;
+	case 3:
+		prize += 'a smartwatch and ';
+	default:
+		prize += 'tickets to the circus.';
+}
+
+console.log("You've won " + prize);
