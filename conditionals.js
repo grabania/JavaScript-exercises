bad4914c71443aa1f9c6d293ed02134ad9067595
grabania <grabania.work@gmail.com>
2020-04-07 19:11:50 +0100

    Update Ice Cream Quiz

--- conditionals.js
+++ conditionals.js
@@ -156,7 +156,11 @@ var flavor = 'strawberry';
 var vessel = 'cone';
 var toppings = 'cookies';
 
-if (flavor === true && vessel === true && toppings === true);
+if (
+	(flavor === 'vanilla' || flavor === 'chocolate') &&
+	(vessel === 'cone' || vessel === 'bowl') &&
+	(toppings === 'sprinkles' || toppings === 'peanuts')
+);
 {
 	console.log(`I'd like two scoops of ${flavor} ice cream in a ${vessel} with ${toppings}.`);
 }
