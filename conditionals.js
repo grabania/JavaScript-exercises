0d488d34082dc50fca53eec47176c6ab8aeb6658
grabania <grabania.work@gmail.com>
2020-04-07 23:27:45 +0100

    Create Card Game

--- conditionals.js
+++ conditionals.js
@@ -225,3 +225,19 @@ var category =
 	eatsPlants && eatsAnimals ? 'omnivore' : eatsAnimals ? 'carnivore' : eatsPlants ? 'herbivore' : undefined;
 
 console.log(category);
+
+var tier = 'nsfw deck';
+var output = 'You’ll receive ';
+
+switch (tier) {
+	case 'deck of legends':
+		output += 'a custom card, ';
+	case "collector's deck":
+		output += 'a signed version of the Exploding Kittens deck, ';
+	case 'nsfw deck':
+		output += 'one copy of the NSFW (Not Safe for Work) Exploding Kittens card game and ';
+	default:
+		output += 'one copy of the Exploding Kittens card game.';
+}
+
+console.log(output);
