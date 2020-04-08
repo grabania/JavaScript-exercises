053cf0fd1c6baa1873270a3c25f5b05088c28df7
grabania <grabania.work@gmail.com>
2020-04-08 19:07:24 +0100

    Create JuliaJames Quiz

--- loops.js
+++ loops.js
@@ -3,3 +3,22 @@ while (x <= 25) {
 	console.log('Printing out x = ' + x);
 	x = x + 2;
 }
+
+/*
+ * Programming Quiz: JuliaJames
+ */
+
+var x = 1;
+
+while (x <= 20) {
+	if (x % 3 === 0 && x % 5 === 0) {
+		console.log('JuliaJames');
+	} else if (x % 5 === 0) {
+		console.log('James');
+	} else if (x % 3 === 0) {
+		console.log('Julia');
+	} else {
+		console.log(x);
+	}
+	x++;
+}
