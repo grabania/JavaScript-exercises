1b58451e6b3638ff5b374e15a9d7d8ecfb458ac8
grabania <grabania.work@gmail.com>
2020-04-08 23:51:05 +0100

    Create Changing the Loop Quiz

--- loops.js
+++ loops.js
@@ -94,3 +94,18 @@ for (var x = 0; x < 5; x = x + 1) {
 		console.log(x + ',' + y);
 	}
 }
+
+/*
+ * Programming Quiz: Changing the Loop.
+ * Rewrite the following while loop as a for loop:
+ */
+
+var x = 9;
+while (x >= 1) {
+	console.log('hello ' + x);
+	x = x - 1;
+}
+
+for (var x = 9; x >= 1; x--) {
+	console.log('hello ' + x);
+}
