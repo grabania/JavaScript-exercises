e6b83dde921b5c31eec4beb8dd9055fb1dcffff4
grabania <grabania.work@gmail.com>
2020-04-08 21:21:45 +0100

    Create 99 Bottles of Juice

--- loops.js
+++ loops.js
@@ -22,3 +22,41 @@ while (x <= 20) {
 	}
 	x++;
 }
+
+/*
+ * Programming Quiz: 99 Bottles of Juice
+ */
+
+var num = 99;
+
+while (num >= 1) {
+	if (num === 2) {
+		console.log(
+			num +
+				' bottles of juice on the wall! ' +
+				num +
+				' bottles of juice! Take one down, pass it around... ' +
+				(num - 1) +
+				' bottle of juice on the wall!'
+		);
+	} else if (num === 1) {
+		console.log(
+			num +
+				' bottle of juice on the wall! ' +
+				num +
+				' bottle of juice! Take one down, pass it around... ' +
+				(num - 1) +
+				' bottles of juice on the wall!'
+		);
+	} else {
+		console.log(
+			num +
+				' bottles of juice on the wall! ' +
+				num +
+				' bottles of juice! Take one down, pass it around... ' +
+				(num - 1) +
+				' bottles of juice on the wall!'
+		);
+	}
+	num--;
+}
