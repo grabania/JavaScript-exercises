5e8d0f4610ce88f95766156f7d8a0ee6bbc28590
grabania <grabania.work@gmail.com>
2020-04-09 19:08:54 +0100

    Add add function

--- functions.js
+++ functions.js
@@ -18,3 +18,13 @@ function findAverage(x, y) {
 }
 
 var avg = findAverage(5, 9);
+
+// x and y are parameters in this function declaration
+function add(x, y) {
+	// function body
+	var sum = x + y;
+	return sum; // return statement
+}
+
+// 1 and 2 are passed into the function as arguments
+var sum = add(1, 2);
