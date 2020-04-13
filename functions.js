d39b8e8104c794b06fc838e6b6e7e1a3c08a829b
grabania <grabania.work@gmail.com>
2020-04-13 15:35:57 +0100

    Create Built a Triangle Quiz

--- functions.js
+++ functions.js
@@ -102,3 +102,25 @@ function sayHi(name) {
 }
 
 sayHi('Julia');
+
+/*
+ * Programming Quiz: Build A Triangle
+ */
+
+function makeLine(length) {
+	var line = '';
+	for (var j = 1; j <= length; j++) {
+		line += '* ';
+	}
+	return line + '\n';
+}
+
+function buildTriangle(x) {
+	var triangle = '';
+	for (var t = 1; t <= x; t++) {
+		triangle += makeLine(t);
+	}
+	return triangle;
+}
+
+console.log(buildTriangle(10));
