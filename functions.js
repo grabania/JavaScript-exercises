6d25bfdf8e3bddf484baa2118881c948b164d1e7
grabania <grabania.work@gmail.com>
2020-04-13 16:15:52 +0100

    Add Inline function expresion

--- functions.js
+++ functions.js
@@ -143,3 +143,33 @@ function helloCat(callbackFunc) {
 
 // pass in catSays as a callback function
 console.log(helloCat(catSays));
+
+// Inline function expressions
+
+// Function expression that assigns the function displayFavorite
+// to the variable favoriteMovie
+var favoriteMovie = function displayFavorite(movieName) {
+	console.log('My favorite movie is ' + movieName);
+};
+
+// Function declaration that has two parameters: a function for displaying
+// a message, along with a name of a movie
+function movies(messageFunction, name) {
+	messageFunction(name);
+}
+
+// Call the movies function, pass in the favoriteMovie function and name of movie
+movies(favoriteMovie, 'Finding Nemo');
+
+//or shorter
+
+// Function declaration that takes in two arguments: a function for displaying
+// a message, along with a name of a movie
+function movies(messageFunction, name) {
+	messageFunction(name);
+}
+
+// Call the movies function, pass in the function and name of movie
+movies(function displayFavorite(movieName) {
+	console.log('My favorite movie is ' + movieName);
+}, 'Finding Nemo');
