9eabcd001796a871a0a8478d3f9893a2c6edd2c7
grabania <grabania.work@gmail.com>
2020-04-14 23:13:55 +0100

    Create The Price is Right

--- arrays.js
+++ arrays.js
@@ -20,3 +20,15 @@ var mechanic = 'Kaylee';
 var crew = [ captain, second, pilot, companion, mercenary, mechanic ];
 
 console.log(crew);
+
+/*
+ * Programming Quiz: The Price is Right
+ */
+
+var prices = [ 1.23, 48.11, 90.11, 8.5, 9.99, 1.0, 1.1, 67.0 ];
+
+prices[0] = 10;
+prices[2] = 20;
+prices[6] = 30;
+
+console.log(prices);
