f3058c90342100cfaddc1510594f445d2d45f3ca
grabania <grabania.work@gmail.com>
2020-04-15 00:02:27 +0100

    Create Joining the Crew Quiz

--- arrays.js
+++ arrays.js
@@ -77,3 +77,24 @@ var team = [
 	'Harry Potter'
 ];
 console.log(hasEnoughPlayers(team));
+
+/*
+ * Programming Quiz: Joining the Crew
+ */
+
+var captain = 'Mal';
+var second = 'Zoe';
+var pilot = 'Wash';
+var companion = 'Inara';
+var mercenary = 'Jayne';
+var mechanic = 'Kaylee';
+
+var crew = [ captain, second, pilot, companion, mercenary, mechanic ];
+
+var doctor = 'Simon';
+var sister = 'River';
+var shepherd = 'Book';
+
+crew.push(doctor, sister, shepherd);
+
+console.log(crew);
