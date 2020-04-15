043233700efa2929c2136f1bf5dc26f149dcd797
grabania <grabania.work@gmail.com>
2020-04-16 00:06:57 +0100

    Add Map Method

--- arrays.js
+++ arrays.js
@@ -175,3 +175,13 @@ test.forEach(function(word) {
 		console.log(word);
 	}
 });
+
+var donuts = [ 'jelly donut', 'chocolate donut', 'glazed donut' ];
+
+var improvedDonuts = donuts.map(function(donut) {
+	donut += ' hole';
+	donut = donut.toUpperCase();
+	return donut;
+});
+
+console.log(improvedDonuts);
