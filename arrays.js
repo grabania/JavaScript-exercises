cb4224f82b795b3e024eda7a88e69e2e189e1c8c
grabania <grabania.work@gmail.com>
2020-04-15 23:38:32 +0100

    Add Arrays Loops

--- arrays.js
+++ arrays.js
@@ -98,3 +98,12 @@ var shepherd = 'Book';
 crew.push(doctor, sister, shepherd);
 
 console.log(crew);
+
+var donuts = [ 'jelly donut', 'chocolate donut', 'glazed donut' ];
+
+// the variable `i` is used to step through each element in the array
+for (var i = 0; i < donuts.length; i++) {
+	donuts[i] += ' hole';
+	donuts[i] = donuts[i].toUpperCase();
+}
+console.log(donuts);
