ba84c67ec0aff5d577a647c96715e2c4d311f876
grabania <grabania.work@gmail.com>
2020-04-17 22:20:22 +0100

    Add Umbrella (open)

---
+++ objects.js
@@ -0,0 +1,13 @@
+var umbrella = {
+	color: 'pink',
+	isOpen: false,
+	open: function() {
+		if (umbrella.isOpen === true) {
+			return 'The umbrella is already opened!';
+		} else {
+			umbrella.isOpen = true;
+			return 'Julia opens the umbrella!';
+		}
+	}
+};
+console.log(umbrella);
