c69007fc36fb98d15c1987a6279cfb801ed2e3bf
grabania <grabania.work@gmail.com>
2020-05-12 20:18:32 +0100

    Create Donuts Revisited Quiz

--- objects.js
+++ objects.js
@@ -208,3 +208,18 @@ let person1 = {
 };
 
 console.log(person1.showInfo());
+
+/*
+ * Programming Quiz: Donuts Revisited
+ */
+
+var donuts = [
+	{ type: 'Jelly', cost: 1.22 },
+	{ type: 'Chocolate', cost: 2.45 },
+	{ type: 'Cider', cost: 1.59 },
+	{ type: 'Boston Cream', cost: 5.99 }
+];
+
+donuts.forEach(function(donut) {
+	console.log(donut.type + ' cost $' + donut.cost + ' each');
+});
